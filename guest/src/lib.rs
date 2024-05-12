59c76af67726fd821a37c9bf01951391c36f7245
Will Papper <contact@papper.me>
2024-05-11 21:57:24 -0700

    feat: Add working UUID generation

--- guest/src/lib.rs
+++ guest/src/lib.rs
@@ -2,11 +2,17 @@
 
 use uuid::Uuid;
 
+// Generates a Version 3 UUID using the DNS namespace and the provided name
 #[jolt::provable]
-fn generate_uuid() -> String {
+fn generate_uuid(name: &[u8]) -> [u8; 16] {
     // uuid v3 is used due to the very strict resource requirements of the zkVM:
     // https://stackoverflow.com/a/20342413
-    Uuid::new_v3(&Uuid::NAMESPACE_DNS, b"hello@example.com").hyphenated().to_string()
+    // name is converted to bytes to be accepted by the Uuid::new_v3 function
+    let uuid = Uuid::new_v3(&Uuid::NAMESPACE_DNS, name);
+    let uuid_bytes = uuid.as_bytes();
+    let mut uuid_fixed_size = [0u8; 16];
+    uuid_fixed_size.copy_from_slice(uuid_bytes);
+    uuid_fixed_size
 }
 
 #[jolt::provable]
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,21 @@
+use uuid::{fmt::Hyphenated, Uuid};
+
 pub fn main() {
     let (prove_fib, verify_fib) = guest::build_fib();
+    let (prove_generate_uuid, verify_generate_uuid) = guest::build_generate_uuid();
 
     let (output, proof) = prove_fib(50);
     let is_valid = verify_fib(proof);
 
     println!("output: {}", output);
+    println!("valid: {}", is_valid);
+
+    let (output, proof) = prove_generate_uuid(b"hello@example.com");
+    let is_valid = verify_generate_uuid(proof);
+
+    println!(
+        "output: {}",
+        Uuid::from_bytes(output).hyphenated().to_string()
+    );
     println!("valid: {}", is_valid);
 }
