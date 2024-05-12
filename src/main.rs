4fce8f14ec8b6151de9fba7b59d2ad90284a5f95
Will Papper <contact@papper.me>
2024-05-11 22:15:41 -0700

    chore: Separate timestamp proving and verification

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,18 @@ use std::time::Instant;
 use uuid::Uuid;
 
 pub fn main() {
-    // Start timing V3 UUID generation
-    let start_v3 = Instant::now();
-
-    // Generate a V3 UUID and prove that it is valid:
+    // Prepare for V3 UUID
     let (prove_generate_uuid_v3, verify_generate_uuid_v3) = guest::build_generate_uuid_v3();
-    let (output_v3, proof_v3) = prove_generate_uuid_v3(b"hello@example.com");
-    let is_valid_v3 = verify_generate_uuid_v3(proof_v3);
 
-    // Calculate elapsed time for V3 UUID generation
-    let duration_v3 = start_v3.elapsed();
+    // Time the proof generation for V3 UUID
+    let start_proof_v3 = Instant::now();
+    let (output_v3, proof_v3) = prove_generate_uuid_v3(b"hello@example.com");
+    let duration_proof_v3 = start_proof_v3.elapsed();
+
+    // Time the verification for V3 UUID
+    let start_verify_v3 = Instant::now();
+    let is_valid_v3 = verify_generate_uuid_v3(proof_v3);
+    let duration_verify_v3 = start_verify_v3.elapsed();
 
     println!(
         "v3 output: {}",
@@ -19,20 +21,26 @@ pub fn main() {
     );
     println!("v3 valid: {}", is_valid_v3);
     println!(
-        "Time taken for V3 UUID generation: {} ms",
-        duration_v3.as_millis()
+        "Time taken for V3 UUID proving: {} ms",
+        duration_proof_v3.as_millis()
+    );
+    println!(
+        "Time taken for V3 UUID verification: {} ms",
+        duration_verify_v3.as_millis()
     );
 
-    // Start timing V5 UUID generation
-    let start_v5 = Instant::now();
-
-    // Generate a V5 UUID and prove that it is valid:
+    // Prepare for V5 UUID
     let (prove_generate_uuid_v5, verify_generate_uuid_v5) = guest::build_generate_uuid_v5();
-    let (output_v5, proof_v5) = prove_generate_uuid_v5(b"hello@example.com");
-    let is_valid_v5 = verify_generate_uuid_v5(proof_v5);
 
-    // Calculate elapsed time for V5 UUID generation
-    let duration_v5 = start_v5.elapsed();
+    // Time the proof generation for V5 UUID
+    let start_proof_v5 = Instant::now();
+    let (output_v5, proof_v5) = prove_generate_uuid_v5(b"hello@example.com");
+    let duration_proof_v5 = start_proof_v5.elapsed();
+
+    // Time the verification for V5 UUID
+    let start_verify_v5 = Instant::now();
+    let is_valid_v5 = verify_generate_uuid_v5(proof_v5);
+    let duration_verify_v5 = start_verify_v5.elapsed();
 
     println!(
         "v5 output: {}",
@@ -40,7 +48,11 @@ pub fn main() {
     );
     println!("v5 valid: {}", is_valid_v5);
     println!(
-        "Time taken for V5 UUID generation: {} ms",
-        duration_v5.as_millis()
+        "Time taken for V5 UUID proving: {} ms",
+        duration_proof_v5.as_millis()
+    );
+    println!(
+        "Time taken for V5 UUID verification: {} ms",
+        duration_verify_v5.as_millis()
     );
 }
