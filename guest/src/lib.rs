c24b9535fd3c35febef954c2aaa00caa579199bf
Will Papper <contact@papper.me>
2024-05-11 21:32:37 -0700

    feat: Compilation of UUID function

--- guest/src/lib.rs
+++ guest/src/lib.rs
@@ -1,5 +1,13 @@
-#![cfg_attr(feature = "guest", no_std)]
 #![no_main]
+
+use uuid::Uuid;
+
+#[jolt::provable]
+fn generate_uuid() -> String {
+    // uuid v3 is used due to the very strict resource requirements of the zkVM:
+    // https://stackoverflow.com/a/20342413
+    Uuid::new_v3(&Uuid::NAMESPACE_DNS, b"hello@example.com").hyphenated().to_string()
+}
 
 #[jolt::provable]
 fn fib(n: u32) -> u128 {
