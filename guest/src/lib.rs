2a21cee6152356a8bdfc5b84c427896fa02e2275
Will Papper <contact@papper.me>
2024-05-11 22:07:23 -0700

    feat: Add support for V5 UUID generation

--- guest/src/lib.rs
+++ guest/src/lib.rs
@@ -4,11 +4,23 @@ use uuid::Uuid;
 
 // Generates a Version 3 UUID using the DNS namespace and the provided name
 #[jolt::provable]
-fn generate_uuid(name: &[u8]) -> [u8; 16] {
+fn generate_uuid_v3(name: &[u8]) -> [u8; 16] {
     // uuid v3 is used due to the very strict resource requirements of the zkVM:
     // https://stackoverflow.com/a/20342413
     // name is converted to bytes to be accepted by the Uuid::new_v3 function
     let uuid = Uuid::new_v3(&Uuid::NAMESPACE_DNS, name);
+    let uuid_bytes = uuid.as_bytes();
+    let mut uuid_fixed_size = [0u8; 16];
+    uuid_fixed_size.copy_from_slice(uuid_bytes);
+    uuid_fixed_size
+}
+
+// Generates a Version 5 UUID using the DNS namespace and the provided name
+#[jolt::provable]
+fn generate_uuid_v5(name: &[u8]) -> [u8; 16] {
+    // uuid v5 is used due to its use of SHA-1, providing better security than MD5:
+    // name is converted to bytes to be accepted by the Uuid::new_v5 function
+    let uuid = Uuid::new_v5(&Uuid::NAMESPACE_DNS, name);
     let uuid_bytes = uuid.as_bytes();
     let mut uuid_fixed_size = [0u8; 16];
     uuid_fixed_size.copy_from_slice(uuid_bytes);
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,27 @@
 use uuid::Uuid;
 
 pub fn main() {
-    let (prove_generate_uuid, verify_generate_uuid) = guest::build_generate_uuid();
+    // Generate a V3 UUID and prove that it is valid:
+    let (prove_generate_uuid_v3, verify_generate_uuid_v3) = guest::build_generate_uuid_v3();
 
-    let (output, proof) = prove_generate_uuid(b"hello@example.com");
-    let is_valid = verify_generate_uuid(proof);
+    let (output_v3, proof_v3) = prove_generate_uuid_v3(b"hello@example.com");
+    let is_valid_v3 = verify_generate_uuid_v3(proof_v3);
 
     println!(
-        "output: {}",
-        Uuid::from_bytes(output).hyphenated().to_string()
+        "v3 output: {}",
+        Uuid::from_bytes(output_v3).hyphenated().to_string()
     );
-    println!("valid: {}", is_valid);
+    println!("v3 valid: {}", is_valid_v3);
+
+    // Generate a V5 UUID and prove that it is valid:
+    let (prove_generate_uuid_v5, verify_generate_uuid_v5) = guest::build_generate_uuid_v5();
+
+    let (output_v5, proof_v5) = prove_generate_uuid_v5(b"hello@example.com");
+    let is_valid_v5 = verify_generate_uuid_v5(proof_v5);
+
+    println!(
+        "v5 output: {}",
+        Uuid::from_bytes(output_v5).hyphenated().to_string()
+    );
+    println!("v5 valid: {}", is_valid_v5);
 }
